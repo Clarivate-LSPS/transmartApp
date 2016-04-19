e63e1009626e834dbedbb246f4ebe8c5ccf172f0
Alexander Bondarev <alexander.bondarev@thomsonreuters.com>
2016-04-19 16:10:16 +0300

    Refactoring for secure concepts controller and uploaded files nodes. Fix obtaining public nodes for non-admin user.

--- grails-app/controllers/SecureConceptsController.groovy
+++ grails-app/controllers/SecureConceptsController.groovy
@@ -1,14 +1,19 @@
-package org.transmart.ontology
-
 import grails.converters.JSON
-
-class ConceptsController {
+/**
+ * Created by transmart on 5/21/14.
+ */
+class SecureConceptsController {
+    def secureConceptsResourceService
     def conceptsResourceService
+    def i2b2HelperService
     def uploadedFilesService
 
     def getCategories() {
         render conceptsResourceService.allCategories as JSON
     }
+
+    def springSecurityService
+    def dataSource
 
     def getChildren() {
         def parentConceptKey = params.get('concept_key') as String
@@ -16,12 +21,14 @@ class ConceptsController {
         def result
         if (!uploadedFilesService.isFilesConcept(parentConceptKey)) {
             result = []
-            def parent = conceptsResourceService.getByKey(parentConceptKey)
-            result.addAll(parent.children)
+            result.addAll(secureConceptsResourceService.getChildConcepts(parentConceptKey))
             result.addAll(uploadedFilesService.getChildConcepts(parentConceptKey))
         } else {
             result = uploadedFilesService.getChildConcepts(parentConceptKey)
         }
+
         render result as JSON
     }
+
+
 }
