3cb4b4a45972496310dcdb9da231e31c7a85fadc
vadim <vadim.ustianskyi@thomsonreuters.com>
2016-09-27 19:49:00 -0700

    No need to restrict the list of accessions especially if NULL value gets filtered out.

--- grails-app/services/com/thomsonreuters/SecureConceptsResourceService.groovy
+++ grails-app/services/com/thomsonreuters/SecureConceptsResourceService.groovy
@@ -44,8 +44,8 @@ class SecureConceptsResourceService {
                       i2b2metadata.i2b2_secure
                     where
                       secure_obj_token = 'EXP:PUBLIC'
-                ) foo where accession in (${(['?'] * accession.size()).join(',')})
-            """, [userid] + accession).accession as Set
+                ) p
+            """, [userid]).accession as Set
         }
 
         result.findAll {
